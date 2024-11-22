22897e7adb200011f6abf6f4a6505c4a5cb8b471
jisu_oh <jisu_oh@tmax.co.kr>
2024-11-22 23:33:02 +0900

    [feat] 홈페이지 get video list 서비스 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 
 import GlobalRoutes from "./Routes/Routes";
 import Loading from "./Pages/Loading";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios.defaults.baseURL = "http://localhost:4000";
+  }, []);
+
   return (
     <Suspense fallback={<Loading />}>
       <GlobalRoutes />
--- src/Pages/Trending.tsx
+++ src/Pages/Trending.tsx
@@ -1,3 +1,9 @@
+import { useEffect } from "react";
+import { videoStore } from "../Stores/video.stores";
+
 export default function Trending() {
+  useEffect(() => {
+    videoStore.getVideoList();
+  }, []);
   return <div>trending</div>;
 }
